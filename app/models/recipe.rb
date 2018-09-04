b5208fbcb7ff7db9de6ee42a4408cd343e1a4e75
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-04 00:15:02 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 class Recipes 
   attr_accessor :name, :ingredients, :cook_time
+  
+  def initialize
+    @name = name
+    @ingredients = ingredients
+    @cook_time = cook_time
+  end
 end
