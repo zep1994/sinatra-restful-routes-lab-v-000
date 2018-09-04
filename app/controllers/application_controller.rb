f1721abe196ef9575851a51c471e76a0a375c983
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-04 02:21:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,9 @@ class ApplicationController < Sinatra::Base
   patch '/recipe/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
-    @recipe.name = params[:name]
-    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
   end
   
 end
