bd4f60a3975b1f959c8a33687b9e2d30e4650d77
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-04 01:12:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/recipes' do
-    @recipe = Recipe.create(new)
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    
   end
 end
