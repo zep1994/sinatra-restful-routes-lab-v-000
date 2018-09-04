aa61d2cb24040e147e8a2a2b4ddf601fda3f4a36
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-04 01:07:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  get '/recipe' do
+    @recipe = recipe
     erb :index
   end
 end
